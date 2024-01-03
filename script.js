4138e4f5f37cd89befd169b0073c852e622addb0
Endale Kinfe <endalekinfe5@gmail.com>
2024-01-03 21:04:15 +0300

    javascript started

---
+++ script.js
@@ -0,0 +1,17 @@
+const textInput = document.getElementById('text-input');
+const checkBtn = document.getElementById('check-btn');
+const result = document.getElementById('result');
+
+function formatInputText(text){
+    const regex = /[,\s_-]/g;
+    return text.replace(regex, "");
+}
+
+function checkTextForPalindrome(){
+
+}
+
+function displayResult(){
+
+}
+
